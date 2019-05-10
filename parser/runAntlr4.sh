d5a5fbf9b43839bd9a0e3d5e8c436f12139fb8a3
RafaAudibert - ML <rafaeelaudibert@gmail.com>
2019-05-10 12:01:11 -0300

    :sparkles: Addes shell script to create ANTLR .py files easier

---
+++ parser/runAntlr4.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+alias antlr4='java -Xmx500M -cp "/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
+alias grun='java -Xmx500M -cp "/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig'
+
+antlr4 -visitor fol.g4
